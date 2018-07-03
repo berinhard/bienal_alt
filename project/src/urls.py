6cea60ddad0a87c6a3801d65965cdeaddb8c9484
Bernardo Fontes <bernardoxhc@gmail.com>
2018-07-03 19:45:06 -0300

    Adiciona página de Sobre

--- project/src/urls.py
+++ project/src/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^sobre/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^tinymce/', include('tinymce.urls')),
     path('', include('src.actions.urls')),
 ]
