5e794290ac598e05c248c2d4c31250319e11a60f
Bernardo Fontes <bernardoxhc@gmail.com>
2018-08-08 16:28:59 -0300

    Serializa a URL raiz do S3 sem a querystring

--- project/src/mce_filebrowser/models.py
+++ project/src/mce_filebrowser/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext as _
 
@@ -14,6 +15,12 @@ class FileBrowserFile(models.Model):
                                      verbose_name = _('File / Image'))
     create_date = models.DateTimeField(auto_now_add=True,
                                        verbose_name=_('Create date'))
+
+    @property
+    def clean_url(self):
+        if settings.PRODUCTION:
+            return self.uploaded_file.url.split('?')[0]
+        return self.uploaded_file.url
 
     def __str__(self):
         return u'{0}'.format(self.uploaded_file.name)
